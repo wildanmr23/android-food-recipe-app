73e4f1e4086a21a1af51d9de1b2d91618b892d1b
wildanmr23 <wildanmursalin.28@gmail.com>
2024-02-17 21:00:36 +0700

    Set Splash Activity

--- app/src/main/java/com/example/foodrecipeapp/views/splash/SplashActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/views/splash/SplashActivity.kt
@@ -3,10 +3,22 @@ package com.example.foodrecipeapp.views.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.foodrecipeapp.R
+import com.example.foodrecipeapp.databinding.ActivitySplashBinding
+import com.example.foodrecipeapp.utils.startActivity
+import com.example.foodrecipeapp.views.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetStarted.setOnClickListener {
+            startActivity<HomeActivity>()
+            finish()
+        }
     }
 }
